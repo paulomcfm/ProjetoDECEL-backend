fba858f24011fa7227321d959d8a6d0db315de54
MuriloMozer <mmozer01@gmail.com>
2024-04-17 23:10:40 -0300

    backend usuario

---
+++ rotas/rotaUsuario.js
@@ -0,0 +1,14 @@
+import {Router} from 'express';
+import UsuarioCtrl from '../controle/usuarioCtrl';
+
+const userCtrl = new UsuarioCtrl();
+const rotaUsuario = new Router();
+
+rotaUsuario.get('/', userCtrl.consultar)
+    .get('/:termo', userCtrl.consultar)
+    .post('/', userCtrl.gravar)
+    .patch('/', userCtrl.atualizar)
+    .put('/', userCtrl.atualizar)
+    .delete('/', userCtrl.excluir);
+
+export default rotaUsuario;
