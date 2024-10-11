f0935ebf233204be60ec17f59ee16aa21852b8ff
Zoratoo <gui_zorato@hotmail.com>
2024-10-11 14:54:46 -0300

    exibir tabela de recebimento no console

--- persistencia/recebimentoDAO.js
+++ persistencia/recebimentoDAO.js
@@ -5,8 +5,10 @@ export default class RecebimentoDAO {
         if (recebimento instanceof Recebimento) {
             const sql = "INSERT INTO recebimentos(rec_mes,rec_ano,rec_valorMensalidade,rec_valorRecebimento,rec_status,rec_tipo,alu_codigo) VALUES($1,$2,$3,$4,$5,$6,$7);";
             const parametros = [recebimento.mes, recebimento.ano, recebimento.valorMensalidade, recebimento.valorRecebimento, recebimento.status, recebimento.tipo, recebimento.aluno.codigo];
-
             await client.query(sql, parametros);
+            const resultado = await client.query("SELECT * FROM recebimentos;");
+            console.log("Tabela de Recebimentos:");
+            console.table(resultado.rows); 
         }
     }
 
